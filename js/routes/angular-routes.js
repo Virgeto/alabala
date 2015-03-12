7b60cbe3a23131e94a35fbf1c3ba151f12a68f41
Vassil Dimitrov <virgeto@gmail.com>
2015-03-12 22:37:04 +0200

    New Partial Under Construction

--- js/routes/angular-routes.js
+++ js/routes/angular-routes.js
@@ -4,7 +4,7 @@ currencyApp.config(function($routeProvider){
                                     .when('/currency',{templateUrl: 'partials/currencyList.html', controller: 'currencyController'})
                                     .when('/news',{templateUrl: 'partials/news.html', controller: 'currencyController'})
                                     .when('/links',{templateUrl: 'partials/links.html', controller: 'currencyController'})
-                                    .when('/contacts',{templateUrl: 'partials/email.html', controller: 'currencyController'})
+                                    .when('/contacts',{templateUrl: 'partials/under_construction.html', controller: 'currencyController'})
                                     .when('/single/:id',{templateUrl: 'partials/singleCurency.html', controller: 'currencyController'})
                                     .otherwise({redirectTo : '/home', templateUrl: 'partials/home.html', controller:'currencyController'});
                                 })
