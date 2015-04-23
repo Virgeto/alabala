4907f233097699b87434bb51f31512d20042c488
Vassil Dimitrov <virgeto@gmail.com>
2015-04-23 09:37:15 +0300

    New html files

--- js/routes/angular-routes.js
+++ js/routes/angular-routes.js
@@ -3,7 +3,7 @@ currencyApp.config(function($routeProvider){
                                     .when('/home',{templateUrl: 'partials/home.html', controller: 'currencyController'})
                                     .when('/currency',{templateUrl: 'partials/currencyList.html', controller: 'currencyController'})
                                     .when('/news',{templateUrl: 'partials/news.html', controller: 'currencyController'})
-                                    .when('/links',{templateUrl: 'partials/links.html', controller: 'currencyController'})
+                                    .when('/links',{templateUrl: 'partials/new.html', controller: 'currencyController'})
                                     .when('/contacts',{templateUrl: 'partials/under_construction.html', controller: 'currencyController'})
                                     .when('/single/:id',{templateUrl: 'partials/singleCurency.html', controller: 'currencyController'})
                                     .otherwise({redirectTo : '/home', templateUrl: 'partials/home.html', controller:'currencyController'});
